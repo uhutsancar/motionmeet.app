976fb56cfb4ccbfa2c662c4047ac022e29f57f0c
Uhut Sancar <sancaruhut@gmail.com>
2025-10-20 12:33:07 +0300

    files created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Layout } from "lucide-react";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Feed from "./pages/Feed";
+import Messages from "./pages/Messages";
+import ChatBox from "./pages/ChatBox";
+import Connections from "./pages/Connections";
+import Discover from "./pages/Discover";
+import Profile from "./pages/Profile";
+import CreatePost from "./pages/CreatePost";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="" element={<Login />}></Route>
+        <Route index element={<Feed />}></Route>
+        <Route path="messages" element={<Messages />}></Route>
+        <Route path="messages/:userId" element={<ChatBox />}></Route>
+        <Route path="connections" element={<Connections />}></Route>
+        <Route path="discover" element={<Discover />}></Route>
+        <Route path="profile" element={<Profile />}></Route>
+        <Route path="profile/:profileId" element={<Profile />}></Route>
+        <Route path="create-post" element={<CreatePost />}></Route>
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
