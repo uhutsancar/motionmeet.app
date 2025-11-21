89b12e3adfb873e728568f568c4c9cc801be9ed5
Uhut Sancar <sancaruhut@gmail.com>
2025-11-21 16:58:16 +0300

    user api created

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import connectDB from "./configs/db.js";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
 import { clerkMiddleware } from '@clerk/express'
+import userRouter from "./routes/userRoutes.js";
+
 
 const app = express();
 
@@ -17,6 +19,7 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => res.send("Server is running"));
 // Set up the "/api/inngest" (recommended) routes with the serve handler
 app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 4000;
 
